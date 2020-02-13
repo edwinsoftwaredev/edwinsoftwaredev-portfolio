511e272bd88a066bd1b25f7392379c70d2c6c1b0
Edwin <edwin_m_paz@hotmail.com>
2020-02-13 12:39:03 -0600

    Update assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,10 +10,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent
-  },
-  {
     path: 'about',
     component: AboutComponent
   },
